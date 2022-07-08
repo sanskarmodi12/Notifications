61a82886519b733aaa0d8ef05947a14a8bbbac43
sanskar <sanskar.modi@telekom-digital.com>
2022-07-08 12:36:02 +0530

    added action bt

--- app/src/main/java/com/example/notifications/MainActivity.java
+++ app/src/main/java/com/example/notifications/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,7 +16,14 @@ public class MainActivity extends AppCompatActivity {
     EditText text_title,text_message;
     Button bt1,bt2;
 
+    public static final String MESSAGE_KEY = "MESSAGE_KEY";
+
     NotificationManager manager;
+
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,25 +37,7 @@ public class MainActivity extends AppCompatActivity {
         text_title=(EditText)findViewById(R.id.text_title);
 
 //        NotificationCompat.Builder builder=new NotificationCompat.Builder(this,App.Channel_One_Id);
-        bt1.setOnClickListener(new View.OnClickListener() {
 
-
-            @Override
-            public void onClick(View v) {
-                String title=text_title.getText().toString();
-                String message=text_message.getText().toString();
-                NotificationCompat.Builder builder=new NotificationCompat.Builder(MainActivity.this,App.Channel_One_Id);
-                builder
-                        .setSmallIcon(R.drawable.ic_baseline_notification_important_24)
-                        .setContentTitle(title)
-                        .setContentText(message)
-                        .setPriority(NotificationCompat.PRIORITY_HIGH)
-                        .build();
-                manager.notify(1, builder.build());
-
-
-            }
-        });
 
         bt2.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -65,5 +57,35 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    public void onb1(View v)
+    {
+        String title=text_title.getText().toString();
+        String message=text_message.getText().toString();
+
+
+
+        Intent contentIntent = new Intent(this, MainActivity.class);
+        PendingIntent contentPendingIntent = PendingIntent.getActivity(
+                this, 0, contentIntent,PendingIntent.FLAG_MUTABLE);
+
+        Intent actionIntent = new Intent(this, SecondActivity.class);
+        actionIntent.putExtra(MESSAGE_KEY, message);
+        PendingIntent actionPendingIntent = PendingIntent.getActivity(this, 0, actionIntent, PendingIntent.FLAG_UPDATE_CURRENT|PendingIntent.FLAG_MUTABLE);
+        NotificationCompat.Builder builder=new NotificationCompat.Builder(MainActivity.this,App.Channel_One_Id);
+        builder
+                .setSmallIcon(R.drawable.ic_baseline_notification_important_24)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setContentIntent(contentPendingIntent)
+                .addAction(R.mipmap.ic_launcher, "Show Toast", actionPendingIntent)
+                .setColor(Color.BLUE)
+                .setAutoCancel(true)
+                .setOnlyAlertOnce(true)
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .build();
+        manager.notify(1, builder.build());
     }
 }
